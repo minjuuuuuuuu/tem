7b5147362c947e3109707c9a2e5824a276937f0f
minjuuuuuuuu <141130022+minjuuuuuuuu@users.noreply.github.com>
2023-08-01 18:51:36 +0900

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    $('.slide').slick({
+        dots: true,
+        infinite: true,
+        // autoplay:true, /*자동재생*/
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
